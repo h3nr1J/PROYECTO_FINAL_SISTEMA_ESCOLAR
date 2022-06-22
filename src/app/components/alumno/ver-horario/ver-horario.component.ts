a522a88fa57871ee6b29aebb265f30704781fc52
JM02022 <018101636j@uandina.edu.pe>
2022-06-22 13:41:29 -0500

    123

--- src/app/components/alumno/ver-horario/ver-horario.component.ts
+++ src/app/components/alumno/ver-horario/ver-horario.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Curso } from 'src/app/models/curso';
+import { Nota } from 'src/app/models/nota';
+import { CursoService } from 'src/app/services/cursos/curso.service';
+import { NotaService } from 'src/app/services/notas/nota.service';
+import { UsuarioService } from 'src/app/services/usuario/usuario.service';
 
 @Component({
   selector: 'app-ver-horario',
@@ -6,10 +11,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ver-horario.component.css']
 })
 export class VerHorarioComponent implements OnInit {
-
-  constructor() { }
+  public cursos:Curso[] = []; 
+  public notas: Nota[] = [];
+  constructor(private usuario:UsuarioService,private apiCurso: CursoService,private notaApi: NotaService) { }
 
   ngOnInit(): void {
+    this.
   }
+  
 
 }
